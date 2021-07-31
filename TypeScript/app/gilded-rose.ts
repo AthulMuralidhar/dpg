5906e3fac38483b366d9dde01f382c2b0dcbd0a1
Athul Muralidhar <athul.m68@gmail.com>
2021-08-01 00:45:47 +0200

    added tests amd made conjured leg work

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -58,20 +58,22 @@ export class GildedRose {
             } else if (item.name.includes('Aged Brie') || item.name.includes('Backstage passes ')) {
                 // console.log('enterting brie / backstage leg')
                 // console.log("item:", item)
-                    item.quality = item.quality + 1
-                    if (item.name.includes('Backstage passes')) {
-                        if (item.sellIn < 11 && item.sellIn >= 0) {
-                            item.quality = item.quality + 1
-                        } else if (item.sellIn < 6 && item.sellIn >= 0) {
-                            item.quality = item.quality + 2
-                        } else if (item.sellIn < 0) {
-                            item.quality = 0
-                        }
+
+                item.quality = item.quality + 1
+                if (item.name.includes('Backstage passes')) {
+                    if ( item.sellIn < 11 && item.sellIn > 6) {
+                        item.quality = item.quality + 1
+                    } else if (item.sellIn < 6 && item.sellIn >= 0) {
+                        item.quality = item.quality + 2
+                    } else if (item.sellIn < 0) {
+                        item.quality = 0
                     }
+                }
                 if (item.quality > 50) {
                     item.quality = 50
                 }
-                    item.sellIn = item.sellIn - 1;
+
+                item.sellIn = item.sellIn - 1;
 
                 // console.log("item:", item)
 
@@ -79,7 +81,7 @@ export class GildedRose {
                 // console.log('enterting normal leg')
                 // console.log("item:", item)
                 if (item.quality > 0 && item.quality <= 50) {
-                    if (item.sellIn < 0) {
+                    if (item.sellIn < 0 || item.name.includes('Conjured')) {
                         item.quality = item.quality - item.quality
                     } else {
                         item.quality = item.quality - 1
--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -3,31 +3,25 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
 
+    // CONSECUTIVE DAYS TEST
     it('should update quality for normal item for 2 consecutive days', function() {
         const gildedRose = new GildedRose([ new Item("+5 Dexterity Vest", 10, 20)]);
         let items = gildedRose.updateQuality();
 
-        // console.log("items", items)
         expect(items[0].name).to.equal('+5 Dexterity Vest');
         expect(items[0].quality).to.equal(19)
         expect(items[0].sellIn).to.equal(9)
 
         items = gildedRose.updateQuality();
 
-        // console.log("items", items)
-
         expect(items[0].name).to.equal('+5 Dexterity Vest');
         expect(items[0].quality).to.equal(18)
         expect(items[0].sellIn).to.equal(8)
     });
-
-
-
     it('should update quality for Brie for 2 consecutive days', function() {
         const gildedRose = new GildedRose( [new Item("Aged Brie", 2, 0)]);
 
         let items = gildedRose.updateQuality();
-
 
         expect(items[0].name).to.equal('Aged Brie');
         expect(items[0].quality).to.equal(1)
@@ -41,12 +35,10 @@ describe('Gilded Rose', function () {
         expect(items[0].sellIn).to.equal(0)
 
     });
-
     it('should update quality for "Backstage passes  for 2 consecutive days - sellin > 10', function() {
         const gildedRose = new GildedRose( [new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)]);
 
         let items = gildedRose.updateQuality();
-
 
         expect(items[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
         expect(items[0].quality).to.equal(21)
@@ -60,12 +52,9 @@ describe('Gilded Rose', function () {
         expect(items[0].sellIn).to.equal(13)
 
     });
-
     it('should update quality for "Backstage passes  for 2 consecutive days - sellin = 10', function() {
-        const gildedRose = new GildedRose( [ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49)]);
-
+        const gildedRose = new GildedRose( [ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 48)]);
         let items = gildedRose.updateQuality();
-
 
         expect(items[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
         expect(items[0].quality).to.equal(50)
@@ -77,34 +66,93 @@ describe('Gilded Rose', function () {
         expect(items[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
         expect(items[0].quality).to.equal(50)
         expect(items[0].sellIn).to.equal(8)
+    });
+    it('should update quality for "Backstage passes  for 2 consecutive days - sellin < 5', function() {
+        const gildedRose = new GildedRose( [ new Item("Backstage passes to a TAFKAL80ETC concert", 4, 47)]);
+        let items = gildedRose.updateQuality();
 
+        expect(items[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
+        expect(items[0].quality).to.equal(50)
+        expect(items[0].sellIn).to.equal(3)
+
+
+        items = gildedRose.updateQuality();
+
+        expect(items[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
+        expect(items[0].quality).to.equal(50)
+        expect(items[0].sellIn).to.equal(2)
+    });
+    it('should update quality for "Sulfuras passes  for 2 consecutive days', function() {
+        const gildedRose = new GildedRose( [new Item("Sulfuras, Hand of Ragnaros", -1, 80)]);
+        let items = gildedRose.updateQuality();
+
+        expect(items[0].name).to.equal('Sulfuras, Hand of Ragnaros');
+        expect(items[0].quality).to.equal(80)
+        expect(items[0].sellIn).to.equal(-1)
+
+
+        items = gildedRose.updateQuality();
+
+        expect(items[0].name).to.equal('Sulfuras, Hand of Ragnaros');
+        expect(items[0].quality).to.equal(80)
+        expect(items[0].sellIn).to.equal(-1)
     });
 
+    // SINGLE INVOKE TESTS
     it('should not update quality for normal item for quality < 0', function() {
         const gildedRose = new GildedRose([ new Item("+5 Dexterity Vest", 10, 0)]);
-
         const items = gildedRose.updateQuality();
-
-        // console.log("items", items)
-
+        
         expect(items[0].name).to.equal('+5 Dexterity Vest');
         expect(items[0].quality).to.equal(0)
+        expect(items[0].sellIn).to.equal(9)
     });
-
     it('should not update quality for normal item for quality > 50', function() {
-        const gildedRose2 = new GildedRose([ new Item("+5 Dexterity Vest", 10, 60)]);
-        const items2 = gildedRose2.updateQuality();
-        expect(items2[0].name).to.equal('+5 Dexterity Vest');
-        expect(items2[0].quality).to.equal(50)
+        const gildedRose = new GildedRose([ new Item("+5 Dexterity Vest", 10, 60)]);
+        const item = gildedRose.updateQuality();
+        expect(item[0].name).to.equal('+5 Dexterity Vest');
+        expect(item[0].quality).to.equal(50)
+        expect(item[0].sellIn).to.equal(9)
     });
-
     it('should not update quality for backstage tickets item for quality > 50', function() {
-        const gildedRose2 = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50)]);
-        const items2 = gildedRose2.updateQuality();
-        expect(items2[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
-        expect(items2[0].quality).to.equal(50)
+        const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50)]);
+        const item = gildedRose.updateQuality();
+        expect(item[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
+        expect(item[0].quality).to.equal(50)
+        expect(item[0].sellIn).to.equal(9)
     });
+    it('should update quality for normal item for sell in < 0', function() {
+        const gildedRose = new GildedRose([ new Item("+5 Dexterity Vest", -1, 50)]);
+        const item = gildedRose.updateQuality();
+        expect(item[0].name).to.equal('+5 Dexterity Vest');
+        expect(item[0].quality).to.equal(0)
+        expect(item[0].sellIn).to.equal(-2)
+    });
+    it('should update quality for Conjured item', function() {
+        const gildedRose = new GildedRose([ new Item("Conjured Mana Cake", 13, 50)]);
+        const item = gildedRose.updateQuality();
+        expect(item[0].name).to.equal('Conjured Mana Cake');
+        expect(item[0].quality).to.equal(0)
+        expect(item[0].sellIn).to.equal(12)
+    });
+    it('should not update quality for backstage tickets item for sellin < 0', function() {
+        const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", -1, 50)]);
+        const item = gildedRose.updateQuality();
+        expect(item[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
+        expect(item[0].quality).to.equal(0)
+        expect(item[0].sellIn).to.equal(-2)
+    });
+    it('should set negative quality for Brie to 0', function() {
+        const gildedRose = new GildedRose([new Item("Aged Brie", 2, -1)]);
 
+        let items = gildedRose.updateQuality();
+
+        expect(items[0].name).to.equal('Aged Brie');
+        expect(items[0].quality).to.equal(0)
+        expect(items[0].sellIn).to.equal(1)
+    })
+// make tests for:
+    // - quality negative for backstage pass
 
 
 });
