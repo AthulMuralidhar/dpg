3d7e1c080d02ed546ebfcb054dcb76ac13a85aad
Athul Muralidhar <athul.m68@gmail.com>
2021-07-31 19:14:10 +0200

    fixed tests and the overall output

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -3,10 +3,13 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+    it('should update quality for normal item', function() {
+        const gildedRose = new GildedRose([ new Item("+5 Dexterity Vest", 10, 20)]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+        expect(items[0].name).to.equal('+5 Dexterity Vest');
+        expect(items[0].quality).to.equal(19)
+        expect(items[0].sellIn).to.equal(9)
     });
+
 
 });
