62ed7dda1e06fc99c8a540da586ae45fd0d8d183
Athul Muralidhar <athul.m68@gmail.com>
2021-08-01 00:57:32 +0200

    removed console logs and comments

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,35 +1,4 @@
-
-/*
-notes
-- in essence this is basically like a system where an item degrades in quality
- the worst quality is on the last day or the sell in day
-- update quality just lowers the quality and sell in date by some ammount
--
-
-conditions:
-- if current date > sell in date then the quality lowers by 2* degrading rate
-- quality should be always >= 0
-- item name: aged brie increases in quality as it sits past the sell in date
-- quality should be always <= 50
-- function updatequality has no effect  on item.name=== Sulfuras
-        - special case "Sulfuras"
-            -  Quality is 80 and it never alters.
-
--  item name = "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
-        - special case for back stage passes:
-            - Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less
-            - but Quality drops to 0 after the concert
-
-
- New feature to impliment:
- - new type of items = conjured items
- conditions:
- - "Conjured" items degrade in Quality twice as fast as normal items
-
-*/
-
 export class Item {
-    // DO NOT CHANGE
     name: string;
     sellIn: number;
     quality: number;
@@ -51,14 +20,8 @@ export class GildedRose {
     updateQuality() {
         for (let item of this.items) {
             if (item.name.includes('Sulfuras')){
-                // console.log('enterting Sulfuras leg')
-                // console.log("item:", item)
                 item.quality = 80
-                // console.log("item:", item)
             } else if (item.name.includes('Aged Brie') || item.name.includes('Backstage passes ')) {
-                // console.log('enterting brie / backstage leg')
-                // console.log("item:", item)
-
                 item.quality = item.quality + 1
                 if (item.name.includes('Backstage passes')) {
                     if ( item.sellIn < 11 && item.sellIn > 6) {
@@ -72,14 +35,8 @@ export class GildedRose {
                 if (item.quality > 50) {
                     item.quality = 50
                 }
-
                 item.sellIn = item.sellIn - 1;
-
-                // console.log("item:", item)
-
             } else {
-                // console.log('enterting normal leg')
-                // console.log("item:", item)
                 if (item.quality > 0 && item.quality <= 50) {
                     if (item.sellIn < 0 || item.name.includes('Conjured')) {
                         item.quality = item.quality - item.quality
@@ -92,7 +49,6 @@ export class GildedRose {
                     item.quality = 50
                 }
                 item.sellIn -= 1
-                // console.log("item:", item)
             }
         }
 
--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -7,29 +7,23 @@ describe('Gilded Rose', function () {
     it('should update quality for normal item for 2 consecutive days', function() {
         const gildedRose = new GildedRose([ new Item("+5 Dexterity Vest", 10, 20)]);
         let items = gildedRose.updateQuality();
-
         expect(items[0].name).to.equal('+5 Dexterity Vest');
         expect(items[0].quality).to.equal(19)
         expect(items[0].sellIn).to.equal(9)
 
         items = gildedRose.updateQuality();
-
         expect(items[0].name).to.equal('+5 Dexterity Vest');
         expect(items[0].quality).to.equal(18)
         expect(items[0].sellIn).to.equal(8)
     });
     it('should update quality for Brie for 2 consecutive days', function() {
         const gildedRose = new GildedRose( [new Item("Aged Brie", 2, 0)]);
-
         let items = gildedRose.updateQuality();
-
         expect(items[0].name).to.equal('Aged Brie');
         expect(items[0].quality).to.equal(1)
         expect(items[0].sellIn).to.equal(1)
 
-
         items = gildedRose.updateQuality();
-
         expect(items[0].name).to.equal('Aged Brie');
         expect(items[0].quality).to.equal(2)
         expect(items[0].sellIn).to.equal(0)
@@ -37,16 +31,12 @@ describe('Gilded Rose', function () {
     });
     it('should update quality for "Backstage passes  for 2 consecutive days - sellin > 10', function() {
         const gildedRose = new GildedRose( [new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)]);
-
         let items = gildedRose.updateQuality();
-
         expect(items[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
         expect(items[0].quality).to.equal(21)
         expect(items[0].sellIn).to.equal(14)
 
-
         items = gildedRose.updateQuality();
-
         expect(items[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
         expect(items[0].quality).to.equal(22)
         expect(items[0].sellIn).to.equal(13)
@@ -55,14 +45,11 @@ describe('Gilded Rose', function () {
     it('should update quality for "Backstage passes  for 2 consecutive days - sellin = 10', function() {
         const gildedRose = new GildedRose( [ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 48)]);
         let items = gildedRose.updateQuality();
-
         expect(items[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
         expect(items[0].quality).to.equal(50)
         expect(items[0].sellIn).to.equal(9)
 
-
         items = gildedRose.updateQuality();
-
         expect(items[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
         expect(items[0].quality).to.equal(50)
         expect(items[0].sellIn).to.equal(8)
@@ -70,14 +57,11 @@ describe('Gilded Rose', function () {
     it('should update quality for "Backstage passes  for 2 consecutive days - sellin < 5', function() {
         const gildedRose = new GildedRose( [ new Item("Backstage passes to a TAFKAL80ETC concert", 4, 47)]);
         let items = gildedRose.updateQuality();
-
         expect(items[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
         expect(items[0].quality).to.equal(50)
         expect(items[0].sellIn).to.equal(3)
 
-
         items = gildedRose.updateQuality();
-
         expect(items[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
         expect(items[0].quality).to.equal(50)
         expect(items[0].sellIn).to.equal(2)
@@ -85,14 +69,11 @@ describe('Gilded Rose', function () {
     it('should update quality for "Sulfuras passes  for 2 consecutive days', function() {
         const gildedRose = new GildedRose( [new Item("Sulfuras, Hand of Ragnaros", -1, 80)]);
         let items = gildedRose.updateQuality();
-
         expect(items[0].name).to.equal('Sulfuras, Hand of Ragnaros');
         expect(items[0].quality).to.equal(80)
         expect(items[0].sellIn).to.equal(-1)
 
-
         items = gildedRose.updateQuality();
-
         expect(items[0].name).to.equal('Sulfuras, Hand of Ragnaros');
         expect(items[0].quality).to.equal(80)
         expect(items[0].sellIn).to.equal(-1)
@@ -100,9 +81,8 @@ describe('Gilded Rose', function () {
 
     // SINGLE INVOKE TESTS
     it('should not update quality for normal item for quality < 0', function() {
-        const gildedRose = new GildedRose([ new Item("+5 Dexterity Vest", 10, 0)]);
+        const gildedRose = new GildedRose([ new Item("+5 Dexterity Vest", 10, -6)]);
         const items = gildedRose.updateQuality();
-        
         expect(items[0].name).to.equal('+5 Dexterity Vest');
         expect(items[0].quality).to.equal(0)
         expect(items[0].sellIn).to.equal(9)
@@ -144,15 +124,9 @@ describe('Gilded Rose', function () {
     });
     it('should set negative quality for Brie to 0', function() {
         const gildedRose = new GildedRose([new Item("Aged Brie", 2, -1)]);
-
         let items = gildedRose.updateQuality();
-
         expect(items[0].name).to.equal('Aged Brie');
         expect(items[0].quality).to.equal(0)
         expect(items[0].sellIn).to.equal(1)
     })
-// make tests for:
-    // - quality negative for backstage pass
-
-
 });
