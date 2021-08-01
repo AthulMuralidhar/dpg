75756c99a3d7393f0436186b8c14d56b7fe4eeff
Athul Muralidhar <athul.m68@gmail.com>
2021-08-01 18:46:43 +0200

    refactored unit tests

---
+++ rust/src/unit_tests.rs
@@ -0,0 +1,15 @@
+use src::gildedrose::{GildedRose, Item};
+
+#[cfg(test)]
+mod tests {
+    // use gildedrose::{GildedRose, Item};
+
+    #[test]
+    pub fn foo() {
+        let items = vec![Item::new("foo", 0, 0)];
+        let mut rose = GildedRose::new(items);
+        rose.update_quality();
+
+        assert_eq!("foo", rose.items[0].name);
+    }
+}
