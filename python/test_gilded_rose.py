3438a9b25fbaa13668af5783fab1dc523c099497
Athul Muralidhar <athul.m68@gmail.com>
2021-08-01 16:44:57 +0200

    python repo chagnes and ts changed
    - added tests for python, added gitignore and poetry to manage modules
    - changed gitignore for typescript

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -4,13 +4,55 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+def test__update_quality_for_normal_item_for_2_consecutive_days():
+    items = [Item("+5 Dexterity Vest", 10, 20)]
 
-        
-if __name__ == '__main__':
-    unittest.main()
+    gilded_rose = GildedRose(items)
+
+    gilded_rose.update_quality()
+
+    assert items[0].name == "+5 Dexterity Vest"
+    assert items[0].sell_in == 9
+    assert items[0].quality == 19
+
+    gilded_rose.update_quality()
+
+    assert items[0].name == "+5 Dexterity Vest"
+    assert items[0].sell_in == 8
+    assert items[0].quality == 18
+
+
+def test__update_quality_for_Brie_for_2_consecutive_days():
+    items = [Item("Aged Brie", 2, 0)]
+
+    gilded_rose = GildedRose(items)
+
+    gilded_rose.update_quality()
+
+    assert items[0].name == "Aged Brie"
+    assert items[0].sell_in == 1
+    assert items[0].quality == 1
+
+    gilded_rose.update_quality()
+
+    assert items[0].name == "Aged Brie"
+    assert items[0].sell_in == 0
+    assert items[0].quality == 2
+
+
+def test__update_quality_for_Backstage_passes_for_2_consecutive_days_sellin_greater_than_10():
+    items = [Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
+
+    gilded_rose = GildedRose(items)
+
+    gilded_rose.update_quality()
+
+    assert items[0].name == "Backstage passes to a TAFKAL80ETC concert"
+    assert items[0].sell_in == 14
+    assert items[0].quality == 21
+
+    gilded_rose.update_quality()
+
+    assert items[0].name == "Backstage passes to a TAFKAL80ETC concert"
+    assert items[0].sell_in == 13
+    assert items[0].quality == 22
